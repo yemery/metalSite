4b257a505422b14f2e7c4d6ae3356787abfc2a2b
yemery <meryemajmani33@gmail.com>
2022-10-21 00:22:27 +0100

    added

---
+++ shop.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style/shops.css">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+       <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">-->
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+       <link rel="preconnect" href="https://fonts.googleapis.com">
+       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+       <link rel = "icon" href ="0.jpg" 
+        type = "image/x-icon" >
+        <title >SLAYER STORE | OFFICIAL MERCH</title>
+    </head>
+    <body>
+       <header>
+         
+           <img src="images/www.webp" width="100%" class="img-fluid" height="100%" alt="">
+           <p > slayer store </p>
+           
+       </header>
+       <section id="store">
+            <div class="container">
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/mmm.webp" alt="">
+                    </div>
+                    
+                        <p>MULTIPLICITY TEE
+
+                        </p>
+                        <p>30$</p>
+                   
+                    
+                </div>
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/ra.webp" alt="">
+                    </div>
+                    
+                        <p>ACID RAIN SEASONS IN THE ABYSS TEE
+
+                        </p>
+                        <p>30$</p>
+                </div>
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/bbb.webp" alt="">
+                    </div>
+                    
+                        <p >SLAYER X MADEWORN 'GLOW LIVE UNDEAD </p>
+                        <p>175$</p>
+               
+                </div>
+
+               
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/ss.webp" alt="">
+                    </div>
+                    
+                        <p>UNDISPUTED ATTITUDE SPIKE TEE
+
+                        </p>
+                        <p>30$</p>
+                </div>
+                <div class="article"> 
+                    <div class="mg">
+                    <img src="images/mp.webp" alt="">
+                </div>
+                
+                    <p>CRUCIFIED DEMON TIE DYE TEE
+
+                    </p>
+                    <p>30$</p>
+                    
+                </div>
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/shp.webp" alt="">
+                    </div>
+                    
+                        <p>HELL AWAITS ENAMEL PIN
+    
+                        </p>
+                        <p>15$</p>
+
+                </div>
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/merch.webp" alt="">
+                    </div>
+                    
+                        <p>KERRY KING STAGE ICON TEE
+                        </p>
+                        <p>25$</p>
+                </div>
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/mer.webp" alt="">
+                    </div>
+                    
+                        <p>TOM ARAYA STAGE ICON TEE
+
+                        </p>
+                        <p>25$</p>
+                </div>
+                <div class="article">
+                    <div class="mg">
+                        <img src="images/j.webp" alt="">
+                    </div>
+                    
+                        <p>JEFF HANNEMAN STAGE ICON TEE
+
+                        </p>
+                        <p>25$</p>
+                </div>
+
+            </div>
+       </section>
+
+        
+    </body>
+</html>
